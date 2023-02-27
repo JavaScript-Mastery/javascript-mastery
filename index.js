2bd08c0e214e459fcdc2d0837a242388719cfdad
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-02-27 21:36:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,138 @@
+// =================================
+// Basic
+// =================================
+
+// let name = "John";
+// const age = 30;
+
+// console.log(name, age);
+
+// =================
+// Output: John 30
+// =================
+// =================================
+//  Functions:
+// =================================
+
+// let num1 = 5;
+// let num2 = 6;
+// console.log(addNumbers(num1, num2));
+// function addNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+// =================
+// Output: 11
+// =================
+// =================================
+// Conditionals:
+// =================================
+
+// const age = 30;
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are not an adult.");
+// }
+// =================
+// Output: You are an adult.
+// =================
+// =================================
+// Loops:
+// =================================
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// =================
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// =================
+// =================================
+// Objects:
+// =================================
+// const person = {
+//   name: "John",
+//   age: 30,
+//   sayHello: function () {
+//     console.log("Hello!");
+//   },
+// };
+// let ans = person;
+// console.log(ans);
+// =================
+// Output: { name: 'John', age: 30, sayHello: [Function: sayHello] }
+// =================
+
+// =========================================
+// operators and expressions in JavaScript
+// =========================================
+
+// ================
+// Operators:
+// ================
+// ================
+// 1. Arithmetic Operators:
+// ================
+// let x = 10;
+// let y = 5;
+
+// console.log(x + y); // Output: 15
+// console.log(x - y); // Output: 5
+// console.log(x * y); // Output: 50
+// console.log(x / y); // Output: 2
+
+// ================
+// 2. Comparison Operators:
+// ================
+// let x = 10;
+// let y = 5;
+
+// console.log(x > y); // Output: true
+// console.log(x < y); // Output: false
+// console.log(x >= y); // Output: true
+// console.log(x <= y); // Output: false
+// console.log(x == y); // Output: false
+// console.log(x != y); // Output: true
+
+// ================
+// 3. Logical Operators:
+// ================
+// let x = 10;
+// let y = 5;
+
+// console.log(x > 5 && y > 5); // Output: false
+// console.log(x > 5 || y > 5); // Output: true
+// console.log(!(x > 5)); // Output: false
+
+// ================
+// 4. Assignment Operators:
+// ================
+
+// let x = 10;
+// x += 5; // Equivalent to x = x + 5
+// console.log(x); // Output: 15
+
+// =================
+// 5. Conditional (Ternary) Operator:
+// =================
+// let x = 10;
+// let y = 5;
+// let result = x > y ? "x is greater than y" : "x is less than or equal to y";
+// console.log(result); // Output: "x is greater than y"
+
+// =================
+// Expressions:
+// =================
+// let x = 10;
+// let y = 5;
+// let z = x + y;
+// console.log(z); // Output: 15
