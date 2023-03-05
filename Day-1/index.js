1ce31c377d4ae94cfe5cc04e00c246c80591f682
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-05 08:20:11 +0530

    Update index.js

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,1 +1,71 @@
+// Get Started Basic of JavaScript Day -1 for introduction
+// Print any name and word/sentence
 
+// console.log("My name is Ajay Dhangar.");
+
+//=======================
+// Variables
+// ======================
+// Step-1:
+// ===============
+// var Name = "Ajay"; //String
+// let Day = 1; // Intiger
+// const Topic = "Introduction of JavaScript"; // string
+
+// console.log("Name:" + Name + " " + "Dhangar");
+// console.log("Day:" + Day);
+// console.log("Topic:" + Topic);
+
+//======================
+// All Operators
+//======================
+// let a = 10;
+// let b = 5;
+// let sum = a + b; // 15
+// let difference = a - b; // 5
+// let product = a * b; // 50
+// let quotient = a / b; // 2
+// let isEqual = a === b; // false
+// let isNotEqual = a !== b; // true
+// let isGreater = a > b; // true
+// let isLess = a < b; // false
+// let andOperator = true && false; // false
+// let orOperator = true || false; // true
+// let notOperator = !true; // false
+
+// console.log("a=" + a + " and b=" + b); // a=10 and b=5
+// console.log("sum=" + sum + " and difference=" + difference); // sum=15 and difference=5
+// console.log("product=" + product + " and quotient=" + quotient); // product=50 and quotient=2
+// console.log("isEqual=" + isEqual + " and isNotEqual=" + isNotEqual); // isEqual=false and isNotEqual=true
+// console.log("isGreater=" + isGreater + " | isLess=" + isLess); // isGreater=true | isLess=false
+// console.log("andOperator=" + andOperator + " | orOperator=" + orOperator); // andOperator=false | orOperator=true
+// console.log(notOperator); // false
+
+// ==================================
+
+// =================================
+
+// let message = "Hello"; // string
+// let age = 22; // integer
+// let isStudent = true; // boolean
+// let person = { name: "Ajay", age: 22 }; // object
+// console.log(message);
+// console.log(age);
+// console.log(isStudent);
+// console.log(person);
+
+//====================
+// Difference between let and const
+//===================
+// let x = 5;
+// x = 10;
+// console.log(x); // 10
+
+// const x = 5;
+// x = 10;
+// console.log(x); //TypeError: Assignment to constant variable.
+
+// var x = 5;
+// x = 10;
+// console.log(x); //5
+//=====================================
