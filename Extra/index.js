7c170bdfd5bc09b77591b33120f5e0502f1ecce3
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-06 22:30:57 +0530

    add pattern

---
+++ Extra/index.js
@@ -0,0 +1,135 @@
+//
+// ======== All * pattern =======
+// ==== pattern: 1 =========
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+//==========================
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 1; col <= 5; col++) {
+//     string += "*" + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+// ============================
+
+// ==== pattern: 2 =========
+//
+// *
+// **
+// ***
+// ****
+// *****
+//===========
+
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = row; col >= 1; col--) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//
+//=================
+// ==== pattern: 3 =========
+// =============
+// *****
+// ****
+// ***
+// **
+// *
+//======
+
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 5; col >= row; col--) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 4 =========
+//===========
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+//============
+
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 1; col <= 5; col++) {
+//     string += col + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 5 =========
+//===========
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = row; col >= 1; col--) {
+//     string += col + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 6 =========
+//===========
+// 5 4 3 2 1
+// 5 4 3 2
+// 5 4 3
+// 5 4
+// 5
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 5; col >= row; col--) {
+//     string += col + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 7 =========
+//===========
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+//===========
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 1; col <= row; col++) {
+//     string += col + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 8 =========
+//===========
+//
+//
+//
+//
+//
+//===========
+
+//============
+// ==== pattern: 9 =========
+//===========
---
+++ Extra/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let string = "";
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; i < 5; j++) {
+    string += j;
+  }
+  string += "\n";
+}
+console.log(string);
