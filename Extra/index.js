24911be08d62a054058490baaa31941a2d15e700
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-07 17:55:24 +0530

    Update index.js

--- Extra/index.js
+++ Extra/index.js
@@ -123,13 +123,51 @@
 //============
 // ==== pattern: 8 =========
 //===========
-//
-//
-//
-//
-//
+// 5 4 3 2 1
+// 4 3 2 1
+// 3 2 1
+// 2 1
+// 1
 //===========
-
+// let string = "";
+// for (let row = 5; row >= 1; row--) {
+//   for (let col = row; col >= 1; col--) {
+//     string += col + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
 //============
-// ==== pattern: 9 =========
+// ==== pattern: 10 =========
 //===========
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+//===========
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = row; col >= 1; col--) {
+//     string += row + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+//============
+// ==== pattern: 10 =========
+//===========
+// 1 1 1 1 1
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4
+// 5 5 5 5 5
+//===========
+// let string = "";
+// for (let row = 1; row <= 5; row++) {
+//   for (let col = 1; col <= 5; col++) {
+//     string += row + " ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
