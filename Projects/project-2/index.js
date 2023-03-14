33be6ba8a9f50c9f064ebc7171aa7807dac14fc0
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-14 20:28:14 +0530

    add new project

--- Projects/project-2/index.js
+++ Projects/project-2/index.js
@@ -6,32 +6,44 @@ let person = document.querySelector(".person");
 
 const quotes = [
   {
-    quote: "1. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"Believe you can and you're halfway there."`,
+    person: "Theodore Roosevelt",
   },
   {
-    quote: "2. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"Success is not final, failure is not fatal: It is the courage to continue that counts."`,
+    person: "Winston Churchill",
   },
   {
-    quote: "3. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"You miss 100% of the shots you don't take."`,
+    person: "Wayne Gretzky",
   },
   {
-    quote: "4. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"The only way to do great work is to love what you do."`,
+    person: "Steve Jobs",
   },
   {
-    quote: "5. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"It does not matter how slowly you go as long as you do not stop."`,
+    person: "Confucius",
   },
   {
-    quote: "6. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"The greatest glory in living lies not in never falling, but in rising every time we fall."`,
+    person: "Nelson Mandela",
   },
   {
-    quote: "7. Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    person: "Ajay",
+    quote: `"Don't watch the clock; do what it does. Keep going."`,
+    person: "Sam Levenson",
+  },
+  {
+    quote: `"If you want to achieve greatness, stop asking for permission."`,
+    person: "Ajay Dhangar",
+  },
+  {
+    quote: `"You can't build a reputation on what you are going to do."`,
+    person: "Henry Ford",
+  },
+  {
+    quote: `"Success is walking from failure to failure with no loss of enthusiasm."`,
+    person: "Winston Churchill",
   },
 ];
 
