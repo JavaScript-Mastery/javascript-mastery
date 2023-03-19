cc59007b348245acba7b76dfe12969f8818d0db5
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-19 17:12:41 +0530

    Confirm

--- Practice/index.js
+++ Practice/index.js
@@ -1,6 +1,121 @@
 // =================================
 // Basic
 // =================================
+// ===================
+// Day-1
+//====================
+
+// =====================================
+//Your first program in JavaScript
+// ======================================
+// console.log("Hello World!");
+
+// ================
+// Data Types and Operators
+//===============
+// 1. Number:
+// ===============
+// let n = 10; //integer type
+// let m = 5.78; // float type
+// console.log(n);
+// console.log(m);
+// ===============
+// 2. String:
+// =============
+// let firstName = "Ajay";
+// let lastName = "Dhangar";
+// let fullName = firstName + " " + lastName;
+// console.log("firstName: " + firstName);
+// console.log("lastName: " + lastName);
+// console.log("fullName: " + fullName);
+
+//================
+//3. Boolean:
+//===============
+
+// let x = 5;
+// let y = 10;
+// console.log(x > y); // false
+// console.log(x < y); // true
+
+//======================
+// 4. Undefined
+//===============
+
+// let a; // a is decleared but not assign any value
+// console.log(a); // undefined
+//=======================
+//5. NULL:
+//=======================
+// let x = null;
+// console.log(x); //null
+
+//==========================
+//Jvascript operators
+//========================
+//1. Arithmetic operators: ( +, -, /, *, %)
+//=======================
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a + b); // 10+5 = 15
+// console.log(a - b); // 10-5 = 5
+// console.log(a * b); // 10x5 = 50
+// console.log(a / b); // 10/5 = 2
+// console.log(a % b); // 10%5 = 0
+// console.log((a + b) * (a - b)); //(15x5) = 75
+
+// ======================
+// 2. Assignment operators:
+//=======================
+
+// let a = 5;
+// a += 10; // a = a+10 = 5+10 =15
+// a = a + 3; // 15 + 3 = 18
+// console.log(a); //18
+//
+// ========================
+// 3. Comparison operators:
+//==========================
+// let a = 5;
+// let b = 10;
+
+// console.log(a > b); // false
+// console.log(a < b); // true
+// console.log(a == b); // false
+// console.log(a === b); //false
+// console.log(a !== b); //true
+// console.log(a <= b); //true
+// console.log(a >= b); //false
+
+//======================
+// Differents between (a==b) and (a===b)
+//========================
+// let a = 4; // number type
+// let b = "4"; // String Type
+// console.log(a == b); // true: (check only value)
+// console.log(a === b); //false: (But It is check value and data type If both are same then print true otherwise print false)
+//
+//====================
+// 4. Logical operators:
+//====================
+
+// let a = 5;
+// let b = 6;
+// console.log(a < 10 || b > 10); // true (OR Logic)
+// console.log(a < 10 && b > 10); // false (AND Logic)
+// console.log(!(a > b)); // true (NOT Logic)
+
+//====================
+// 5. String operators:
+//====================
+
+// let fName = "Ajay";
+// let lName = "Dhangar";
+
+// let fullName = fName + " " + lName;
+// console.log(fullName); // Ajay Dhangar
 
 // let name = "John";
 // const age = 30;
@@ -339,3 +454,486 @@
 // }
 // console.log(string);
 //==============================
+
+//
+// ===================
+//   functions
+// ===================
+// const str = "Hello, world!";
+
+// function sayHello() {
+//   console.log(str);
+// }
+// sayHello(); // Output: Hello, world!
+//
+// ========================
+
+// function sum(x, y) {
+//   return x + y;
+// }
+// console.log("Sum = " + sum(4, 5)); // Sum = 9
+
+// ==================
+
+// let a = parseInt(prompt("Enter a num1: "));
+// let b = parseInt(prompt("Enter a num2: "));
+
+// const result = multipli(a, b);
+
+// console.log(
+//   "Multiplication of " + `${a}` + " and " + `${b}` + " is " + `${result}`
+// );
+// for example a=9, b=8 then,
+// Multiplication of 9 and 8 is 72
+
+// function multipli(x, y) {
+//   return x * y;
+// }
+
+// =================
+//  Conditions
+// =================
+// =================
+// 1. if...else
+// ==============
+
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are a minor.");
+// }
+
+// ===========================
+// =================
+// 2. Switch
+// =================
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday.");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday.");
+//     break;
+//   case "Wednesday":
+//     console.log("Today is Wednesday.");
+//     break;
+//   default:
+//     console.log("I don't know what day it is.");
+// }
+// ========================================================
+//
+// Day-3 Arrays and Loops in  JavaScript
+
+// Arrays:
+//========================
+// 1-D array
+//========================
+// let MyArray = [1, 2, "name", 2.0, "demo"];
+// console.log(MyArray); // output: [1, 2, "name", 2.0, "demo"]
+
+// ====== manipulate Arry ============
+
+//let MyArray = [1, 2, "name", 2.0, "demo"];
+
+// MyArray.push(6); // increase index and add new value 6
+
+// console.log(MyArray); // output: [1, 2, "name", 2.0, "demo", 6]
+
+// MyArray.pop(); // Decrease index and remove last value 6
+
+// console.log(MyArray); // output: [1, 2, "name", 2.0, "demo"]
+
+// MyArray.shift(); // remove index 1 or myArray[0]
+// console.log(MyArray); // Output: [ 2, 'name', 2, 'demo' ]
+// MyArray.unshift(0);
+// console.log(MyArray); // Output: [ 0, 2, 'name', 2, 'demo' ]
+// MyArray.splice(2, 1);
+// console.log(MyArray); //Output:  [ 0, 2, 2, 'demo' ]
+//=================================
+// ====== loops =========
+//=================================
+
+let myArray = [1, 2, 3, 4, 5];
+
+// for loop
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// for..in loop
+// for (let index in myArray) {
+//   console.log(myArray[index]);
+// }
+
+// for..of loop
+// for (let element of myArray) {
+//   console.log(element);
+// }
+
+// while loop
+// let i = 0;
+// while (i < myArray.length) {
+//   console.log(myArray[i]);
+//   i++;
+// }
+
+// do..while loop
+// let j = 0;
+// do {
+//   console.log(myArray[j]);
+//   j++;
+// } while (j < myArray.length);
+
+// nested loop
+// let multiArray = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+
+// for (let i = 0; i < multiArray.length; i++) {
+//   for (let j = 0; j < multiArray[i].length; j++) {
+//     console.log("multiArray [" + i + j + "] = " + multiArray[i][j]);
+//   }
+// }
+
+//==================     ===================
+// // Creating an object with properties
+// const person = {
+//   name: "John",
+//   age: 30,
+//   gender: "male",
+//   occupation: "developer",
+// };
+
+// // Accessing properties using dot notation
+// console.log(person.name); // Output: 'John'
+// console.log(person.age); // Output: 30
+
+// // Accessing properties using bracket notation
+// console.log(person["gender"]); // Output: 'male'
+// console.log(person["occupation"]); // Output: 'developer'
+
+// // Adding new properties
+// person.email = "john@example.com";
+
+// // Modifying existing properties
+// person.age = 31;
+
+// // Deleting properties
+// delete person.email;
+
+// // Checking if a property exists
+// console.log("gender" in person); // Output: true
+// console.log("email" in person); // Output: false
+
+// // Looping through all properties of an object
+// for (let key in person) {
+//   console.log(key + ": " + person[key]);
+// }
+
+// function countAlphabets(str, n) {
+//   // initialize an empty map
+//   let map = new Map();
+
+//   // iterate over the string
+//   for (let i = 0; i < n; i++) {
+//     // convert to lowercase to avoid case sensitivity
+//     let char = str.charAt(i).toLowerCase();
+
+//     // if the character is an alphabet, update the map
+//     if (/[a-z]/.test(char)) {
+//       if (map.has(char)) {
+//         map.set(char, map.get(char) + 1);
+//       } else {
+//         map.set(char, 1);
+//       }
+//     }
+//   }
+
+//   // print the count of each alphabet
+//   for (let [key, value] of map) {
+//     console.log(`${key}: ${value}`);
+//   }
+// }
+
+// // prompt the user to enter a string and a length
+// let str = prompt("Enter a string:");
+// let n = str.length;
+
+// // call the countAlphabets function with the user input
+// countAlphabets(str, n);
+//===================================
+// create obejects & properties
+// const person = {
+//   name: "Ajay Dhangar",
+//   age: 22,
+//   about: "Web Developer",
+//   hobbies: "Coding",
+// };
+// We can access properties with the help of . notation and bracket notation
+
+// console.log(person.name);
+// console.log(person["age"]);
+
+// Now, add new properties with the help of . notation and bracket notation
+// person.email = "info@gmail.com";
+
+// person.phone_no = 1234567890;
+// console.log(person.email);
+// console.log(person.phone_no);
+// Delete properties
+// delete person.email;
+// console.log(person.email);
+// console.log("--------------------");
+// update properties
+// person.age = 20;
+// if you print all with hte help of looping
+// for (let key in person) {
+//   console.log(key + ":" + person[key]);
+// }
+
+//============================
+
+// let object1 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+// let object2 = {
+//   a: 5,
+//   d: 6,
+//   e: 7,
+// };
+// let object3 = {
+//   f: 8,
+//   g: 9,
+//   h: 10,
+// };
+// console.log(object1); // {a: 1, b: 2, c: 3}
+// console.log(object1.a); // 1
+// console.log(object2.a); // 5
+
+//============================
+// this (keyword)
+// =================
+
+// let newObject = {
+//   firstname: "Ajay",
+//   lastname: "Dhangar",
+//   Age: 22,
+//   print: function () {
+//     console.log(
+//       "Name: " +
+//         this.firstname +
+//         "-" +
+//         this.lastname +
+//         " | " +
+//         "age: " +
+//         this.Age
+//     );
+//   },
+// };
+// newObject.print(); //Name: Ajay-Dhangar | age: 22
+
+// =====================================
+// function addItem() {
+//   var input = document.getElementById("input");
+//   var text = input.value;
+//   if (text === "") {
+//     alert("Please enter an item.");
+//   } else {
+//     var item = document.createElement("li");
+//     var itemText = document.createTextNode(text);
+//     item.appendChild(itemText);
+//     document.getElementById("list").appendChild(item);
+//     input.value = "";
+//   }
+// }
+
+// document.getElementById("list").addEventListener("click", function (e) {
+//   if (e.target && e.target.nodeName == "LI") {
+//     e.target.parentNode.removeChild(e.target);
+//   }
+// });
+// ============================================
+// const heading = document.getElementById("my-heading");
+// const button = document.getElementById("my-button");
+// const head = document.getElementById("my-name");
+
+// button.addEventListener("click", function () {
+//   heading.innerHTML = "Button clicked";
+//   head.innerHTML = " By You ";
+//   heading.style.color = "red";
+//   head.style.color = "green";
+// });
+// var elements = document.getElementsByClassName("example");
+
+// // Loop through the elements and change their text color
+// for (var i = 0; i < elements.length; i++) {
+//   if (i % 2 == 0) {
+//     elements[i].style.color = "red";
+//   } else {
+//     elements[i].style.color = "blue";
+//   }
+// }
+// var element = document.getElementsByTagName("div");
+// for (var i = 0; i < elements.length; i++) {
+//   if (i % 2 == 0) {
+//     element[i].style.background = "#11f4ff";
+//   } else {
+//     element[i].style.background = "#fff411";
+//   }
+//   element[i].style.height = "50px";
+// }
+// ================================================
+// function divide(a, b) {
+//   if (b === 0) {
+//     throw new Error("Cannot divide by zero");
+//   }
+//   return a / b;
+// }
+
+// try {
+//   console.log(divide(10, 0));
+// } catch (error) {
+//   console.error(error.message);
+// }
+//=============================================
+// ===== 1. Creating a regex object ======
+
+// const regex = /pattern/;
+
+// console.log(regex);
+
+//=========================
+
+// ========== 2. Matching literal text =======
+// const regex = /hello/;
+// const result = regex.test("hello world"); // returns true
+// console.log(result);
+
+//==============================
+
+// ==== 3. Matching character sets ======
+
+// const regex = /[abc]/;
+// const result1 = regex.test("a"); // returns true
+// const result2 = regex.test("d"); // returns false
+// console.log(result1);
+// console.log(result2);
+
+// ==== OR =========
+// const regex = /[0-9]/;
+// const result1 = regex.test("5"); // returns true
+// const result2 = regex.test("x"); // returns false
+// console.log(result1);
+// console.log(result2);
+//==============================
+
+//===== 4. Matching metacharacters===========
+
+// const regex = /h.t/;
+// const result1 = regex.test("hot"); // returns true
+// const result2 = regex.test("hat"); // returns true
+// const result3 = regex.test("h\nt"); // returns false
+// const result4 = regex.test("hut"); // returns true
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+
+//========== OR =============
+
+// const regex = /^hello$/;
+// const result1 = regex.test("hello"); // returns true
+// const result2 = regex.test("hello world"); // returns false
+// console.log(result1);
+// console.log(result2);
+
+// ========================
+
+// ====== 5. Matching quantifiers======
+
+// const regex = /ab*c/;
+// const result1 = regex.test("ac"); // returns true
+// const result2 = regex.test("abc"); // returns true
+// const result3 = regex.test("abbbc"); // returns true
+// const result4 = regex.test("abxyc"); // returns false
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+
+// =========================
+
+// ======= 6. Matching with anchors ========
+
+// const regex = /\bhello\b/;
+// const result1 = regex.test("hello"); // returns true
+// const result2 = regex.test("hello world"); // returns true
+// const result3 = regex.test("sayhello"); // returns false
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+// =====================
+
+// ==== 7. Capturing groups ======
+
+// const regex = /(\d{3})-(\d{3})-(\d{4})/;
+// const input = "My phone number is 123-456-7890";
+// const match = input.match(regex);
+// console.log(match); // returns ["123-456-7890", "123", "456", "7890"]
+
+//================================
+// ===== 8. Non-capturing groups =====
+
+// const regex = /(?:https?:\/\/)?(www\.)?google\.com/;
+// const result1 = regex.test("http://www.google.com"); // returns true
+// const result2 = regex.test("https://google.com"); // returns true
+// const result3 = regex.test("www.google.com"); // returns true
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+//===============================
+// =====  9. Lookahead and lookbehind assertions =====
+
+// // Positive lookahead assertion
+// const regex1 = /foo(?=bar)/;
+// const result1 = regex1.test("foobar"); // returns true
+// const result2 = regex1.test("foobaz"); // returns false
+
+// // Negative lookahead assertion
+// const regex2 = /foo(?!bar)/;
+// const result3 = regex2.test("foobar"); // returns false
+// const result4 = regex2.test("foobaz"); // returns true
+
+// // Positive lookbehind assertion
+// const regex3 = /(?<=foo)bar/;
+// const result5 = regex3.test("foobar"); // returns true
+// const result6 = regex3.test("bazbar"); // returns false
+
+// // Negative lookbehind assertion
+// const regex4 = /(?<!foo)bar/;
+// const result7 = regex4.test("foobar"); // returns false
+// const result8 = regex4.test("bazbar"); // returns true
+
+// console.log("result1 : " + result1);
+// console.log("result2 : " + result2);
+// console.log("result3 : " + result3);
+// console.log("result4 : " + result4);
+// console.log("result5 : " + result5);
+// console.log("result6 : " + result6);
+// console.log("result7 : " + result7);
+// console.log("result8 : " + result8);
+
+// =========================
